529ced1491d6c0e824a475fb8159ae8e03d05992
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-12-23 19:37:02 +0300

    + clean_logs

--- simple_script.py
+++ simple_script.py
@@ -1,6 +1,13 @@
-def main():
-    ...
+import sys
+
+
+def clean_logs():
+    if len(sys.argv) != 3:
+        sys.exit(1)
+    log_dir = sys.argv[1]
+    days = sys.argv[2]
+    print(f"Сканируем директорию {log_dir} на файлы старше {days} дней...")
 
 
 if __name__ == "__main__":
-    main()
+    clean_logs()
