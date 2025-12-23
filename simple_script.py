47c7ece9f637b99b02390cb52b5fad878bea2dc3
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-12-23 20:29:11 +0300

    clean_logs 1.1

--- simple_script.py
+++ simple_script.py
@@ -1,3 +1,5 @@
+import os
+import time
 import sys
 
 
@@ -5,8 +7,21 @@ def clean_logs():
     if len(sys.argv) != 3:
         sys.exit(1)
     log_dir = sys.argv[1]
-    days = sys.argv[2]
-    print(f"Сканируем директорию {log_dir} на файлы старше {days} дней...")
+    threshold = time.time() - (int(sys.argv[2]) * 86400)
+    files_to_delete = []
+
+    if not os.path.isdir(log_dir):
+        print(f"Ошибка: Директория {log_dir} не найдена.")
+        return
+
+    for filename in os.listdir(log_dir):
+        if filename.endswith(".log"):
+            file_path = os.path.join(log_dir, filename)
+            if os.path.getmtime(file_path) < threshold:
+                files_to_delete.append(file_path)
+
+    for f in files_to_delete:
+        print(f"Найден: {f}")
 
 
 if __name__ == "__main__":
